e1f53fb27cfe99f2558895e52aaad6b466ce1215
Jordan Bursik <jsbursik@gmail.com>
2025-10-06 15:48:22 -0500

    Swapping to postgres - need faster queries & features

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "file:./data/dev.db",
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
